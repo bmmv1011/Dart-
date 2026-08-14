59e48273682aab0b6b39b2e8d48eb54a6cc71d16
bmmv1011 <bmmv.10.11@gmail.com>
2026-08-14 12:49:43 +0530

    Create grade.dart

---
+++ grade.dart
@@ -0,0 +1,16 @@
+void main() {
+  int marks = 17;
+
+  // Grade evaluation
+  if (marks >= 75) {
+    print("A");
+  } else if (marks >= 65) {
+    print("B");
+  } else if (marks >= 55) {
+    print("C");
+  } else if (marks >= 45) {
+    print("S");
+  } else {
+    print("W");
+  }
+}
