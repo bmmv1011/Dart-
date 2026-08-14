760b8daaf8520a1566ae57cf059d42d84ccbea99
bmmv1011 <bmmv.10.11@gmail.com>
2026-08-14 12:31:59 +0530

    Create waterlevel.dart

---
+++ waterlevel.dart
@@ -0,0 +1,16 @@
+void main() {
+    int waterLevel = 30;
+
+    if (waterLevel == 10) {
+        //statement for waterlevel = 10
+        print("Light the PINK BULB");
+    }
+    if (waterLevel == 20) {
+        //statement for waterlevel = 20
+        print("Light the BLUE BULB");
+    }
+    if (waterLevel == 30) {
+        //statement for waterlevel = 30
+        print("Light the GREEN BULB");
+    }
+}
