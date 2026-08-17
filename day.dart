954fe91cd67bf1ac512f8a995d121645bb4997c3
Senuth <rajapakshasenu4@gmail.com>
2026-08-17 10:31:07 +0530

    Home work

---
+++ day.dart
@@ -0,0 +1,25 @@
+void main(){
+String day ="Monday";
+if(day == "Monday"){
+  String week = "weekday";
+}
+if else(day == "Tuseday"){
+  String week = "weekday";
+}
+if else(day == "Wednesday"){
+  String week = "weekday";
+}
+if else(day == "Thersday"){
+  String week = "weekday";
+}
+if else(day == "Friday"){
+  String week = "weekday";
+}
+if else(day == "Saterday"){
+  String week = "weekend";
+}
+else(day == "Sunday"){
+  String week = "weekend";
+}
+print(week);
+}
---
+++ wether.dart
@@ -0,0 +1,16 @@
+void main(){
+ String wether = "sunny";
+ if(wether == "sunny"){
+  print("It's a sunny day!");
+ } else if(wether == "rainy"){
+  print("It's raining outside.");
+ }
+ else if(wether == "cloudy"){
+  print("It's a cloudy day.");
+ } else if(wether == "snowy"){
+  print("It's snowing outside.");
+ } else (wether == "windy"){
+  print("It's windy outside.");
+
+
+}
